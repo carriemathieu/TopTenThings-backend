545ff04886d72fa701d4ba21ea5372f85f514765
Carrie Mathieu <carrieburks@gmail.com>
2021-05-12 21:53:55 -0400

    user seed data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,17 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
-  # # GET /users
-  # def index
-  #   @users = User.all
+  # GET /users
+  def index
+    @users = User.all
 
-  #   render json: @users
-  # end
+    render json: @users
+  end
 
-  # # GET /users/1
-  # def show
-  #   render json: @user
-  # end
+  # GET /users/1
+  def show
+    render json: @user
+  end
 
   # POST /users
   def create
