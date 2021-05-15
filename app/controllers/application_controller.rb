4fff8e2d791256e4567225c426227de858578892
Carrie Mathieu <carrieburks@gmail.com>
2021-05-14 22:11:21 -0400

    test helper methods for user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+    helpers do
+        # test for log in
+        def current_user
+            User.first
+        end
+
+        def logged_in?
+            !!current_user
+        end
+    end
 end
