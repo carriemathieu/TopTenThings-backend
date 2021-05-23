e9fa8d7b08850534f56704424d1e00c57cd993df
Carrie Mathieu <carrieburks@gmail.com>
2021-05-22 21:43:01 -0400

    sessions controller -> create

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,16 @@
 class Api::V1::SessionsController < ApplicationController
 
     def create
+        @user = User.find_by(email: params[:session][:email])
 
+        if @user && @user.authenticate(params[:session][:password])
+            session[:user_id] = @user.id
+            render json: @user
+        else
+            render json: {
+                error: "Invalid Credentials"
+            }
+        end
     end
     
     def
