8082c13f976967d7433cd6883deca3d187dc49de
Carrie Mathieu <carrieburks@gmail.com>
2021-05-28 22:15:55 -0400

    updated serializers

--- app/serializers/lists_serializer.rb
+++ app/serializers/lists_serializer.rb
@@ -1,4 +1,11 @@
 class ListsSerializer
   include FastJsonapi::ObjectSerializer
   attributes :list_title :list_content :category :user
+
+  attribute :locations_json do |trip|
+    trip.locations.map{|loc| {
+      city: loc.city
+      city: loc.state,
+      city: loc.country
+    }}
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,7 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :first_name, :last_name, :email, :lists, :categories
+  has_many :lists, serializer: ListsSerializer
+
+  
 end
