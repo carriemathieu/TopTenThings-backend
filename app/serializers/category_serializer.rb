7934810831da718428aec4c65e3315e325e17bfb
Carrie Mathieu <carrieburks@gmail.com>
2021-05-28 21:40:07 -0400

    added comment to category serializer

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -2,6 +2,7 @@ class CategorySerializer
   include FastJsonapi::ObjectSerializer
   attributes :name :user
 
+  # belongs_to :hometown, serializer: HometownSerializer
   # attribute :hometown do |user|
   #   {
   #     city: user.hometown.city,
