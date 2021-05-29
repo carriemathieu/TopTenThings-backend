ce2128ea50a27930162a4f17effdd2e6ce521196
Carrie Mathieu <carrieburks@gmail.com>
2021-05-28 22:16:34 -0400

    commented out location in lists serializer

--- app/serializers/lists_serializer.rb
+++ app/serializers/lists_serializer.rb
@@ -2,10 +2,10 @@ class ListsSerializer
   include FastJsonapi::ObjectSerializer
   attributes :list_title :list_content :category :user
 
-  attribute :locations_json do |trip|
-    trip.locations.map{|loc| {
-      city: loc.city
-      city: loc.state,
-      city: loc.country
-    }}
+  # attribute :locations_json do |trip|
+  #   trip.locations.map{|loc| {
+  #     city: loc.city
+  #     city: loc.state,
+  #     city: loc.country
+  #   }}
 end
