af8c8277a2c83608150f6d0dae37f63852c9674c
Carrie Mathieu <carrieburks@gmail.com>
2021-06-25 18:58:48 -0400

    add validation on user model for first name, last name, email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
     has_many :lists
     has_many :categories, through: :lists
     has_secure_password
+
+    validates :first_name, :last_name, :email, presence: true
 end
