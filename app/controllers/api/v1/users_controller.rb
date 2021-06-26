e0093d420db4e7e65209c5ff4420ec85eff7ed4c
Carrie Mathieu <carrieburks@gmail.com>
2021-06-26 00:13:14 -0400

    add user session

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -20,6 +20,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       render json: UserSerializer.new(@user), status: :created
     else
       resp = {
